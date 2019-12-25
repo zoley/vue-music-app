5d14efd5abe8e42fcc6581de74775a6e0ba26998
zoley <zole1314@163.com>
2019-12-25 23:33:00 +0800

    初始导入

--- .history/.eslintrc_20191225230512.js
+++ .history/.eslintrc_20191225230512.js
@@ -9,9 +9,7 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "no-extra-semi": 2, //不允许出现不必要的分号
-    "semi": 0, //语句分号结尾，加不加都行
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
--- .history/.eslintrc_20191225231803.js
+++ .history/.eslintrc_20191225231803.js
@@ -11,7 +11,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "no-extra-semi": 2, //不允许出现不必要的分号
-    "semi": 0, //语句分号结尾，加不加都行
   },
   parserOptions: {
     parser: 'babel-eslint'
