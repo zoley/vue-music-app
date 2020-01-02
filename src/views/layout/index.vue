97db0f78853abfc28936fdbe0b4c6f5daa0b6424
zoley <zole1314@163.com>
2020-01-02 18:16:35 +0800

    0.1 换肤

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -6,6 +6,7 @@
       <router-link class="nav-item" to="/ranking">排行</router-link>
       <router-link class="nav-item" to="/search">搜索</router-link>
     </div>
+    <p @click="changeTheme">啊啊啊啊啊啊变色</p>
     <div class="content">
       <keep-alive>
         <router-view></router-view>
@@ -31,18 +32,25 @@ export default {
       request.get('/api/seller').then(res => {
         this.seller = Object.assign({}, res.data);
       });
+    },
+    /**
+     * 改变主题
+     */
+    changeTheme () {
+      window.document.documentElement.setAttribute('data-theme', 'theme1');
     }
   }
 };
 </script>
 <style scoped lang="scss">
+@import '../../assets/css/mixin';
 .layout {
   max-width: 1024px;
   margin: 0 auto;
+  @include bg_color($background-color-theme);
   .nav {
     width: 100%;
     height: 40px;
-    background: white;
     border-bottom: 1px solid rgba(7, 17, 27, 0.1);
     box-sizing: border-box;
     .nav-item {
@@ -53,6 +61,7 @@ export default {
       text-decoration: none;
       height: 100%;
       line-height: 40px;
+      @include font_color($font-color-theme);
       &.router-link-active {
         color: rgb(240, 20, 20);
       }
