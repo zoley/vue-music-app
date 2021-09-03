769b079b8325d48db27bd39bf6f7d31c39df97be
zoley <zole1314@163.com>
2021-09-03 17:10:14 +0800

    search-box

---
+++ src/components/SearchBox/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="search-box z-flex">
+    <div class="input-box">
+      <input v-model="searchKey" type="search" class="input" placeholder="请输入歌手、歌曲名" @keyup.enter="doSearch">
+      <i v-if="searchKey" class="iconfont i-close gray-color" @click="clearKey" />
+    </div>
+    <button v-ripple="{color:'var(--font-color-active)'}" class="btn" @click="doSearch">
+      搜索
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBox',
+  data() {
+    return {
+      searchKey: ''
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    /**
+     * 清除搜索词
+     */
+    clearKey() {
+      this.searchKey = ''
+    },
+    /**
+     * 搜索
+     */
+    doSearch() {
+      this.$emit('sendSearch', this.searchKey)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .search-box{
+    padding:$xs 0;
+    width:100%;
+    box-sizing: border-box;
+    justify-content: space-between;
+    .input-box{
+      position:relative;
+      flex:1;
+      padding-left:$sm;
+      .input{
+        border-radius:32px;
+        background: var(--bg-color-sec);
+        border:1px solid var(--bg-color-sec);
+        padding:$xxs $sm;
+        height:32px;
+        width:100%;
+        color:var(--font-color-sec);
+        box-sizing: border-box;
+        &:focus{
+          border-color: var(--font-color-active);
+        }
+      }
+      .iconfont{
+        font-size:$font_small;
+        position:absolute;
+        right:$sm;
+        top:$xs;
+      }
+    }
+    .btn{
+      width:60px;
+      line-height: 32px;
+      height:32px;
+      text-align: center;
+      color:var(--font-color-active);
+      font-size:$font_small;
+      border:none;
+      background:transparent;
+    }
+  }
+</style>
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>搜索</div>
+  <div class="search-wrap">
+    <search-box @sendSearch="doSearch" />
+    <div class="history">
+      <h3 class="history-name z-flex">
+        <span>搜索历史</span>
+        <i class="iconfont i-shanchu" />
+      </h3>
+      <ul>
+        <li v-for="(item,index) in historyList" :key="index">
+          <span>{{ item }}</span>
+          <i class="iconfont i-close" />
+        </li>
+      </ul>
+    </div>
+    <div v-show="keywords" class="search-content">
+      内容
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Search',
+  data() {
+    return {
+      keywords: '',
+      historyList: ['周杰伦', '我在那一角落患过伤风']
+    }
+  },
+  mounted() {},
+  methods: {
+    /**
+     * 搜索
+     */
+    doSearch(val) {
+      this.keywords = val
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .search-wrap{
+    .search-content{
+      height: calc(100vh - 84px - 60px);
+      overflow: hidden;
+    }
+  }
 </style>
